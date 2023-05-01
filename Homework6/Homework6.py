dbfe64624bcb11cf2ccd243360f37b788d63b63d
Teymur <teymur_aliyev@hotmail.com>
2023-05-01 14:35:39 +0400

    Add project files.

---
+++ Homework6/Homework6.py
@@ -0,0 +1,31 @@
+#Task 30
+
+
+
+#a1 = int(input("Enter the first element of the progression: "))
+#d = int(input("Enter the progression difference: "))
+#n = int(input("Enter the number of elements of the progression: "))
+
+#arr = []
+#for i in range(n):
+#    an = a1 + i * d
+#    arr.append(an)
+
+#print("An array filled with arithmetic progression elements: ")
+#print(arr)
+
+
+#Task 32
+
+#def find_indexes(lst, minimum, maximum):
+#    indexes = []
+#    for i in range(len(lst)):
+#        if minimum <= lst[i] <= maximum:
+#            indexes.append(i)
+#    return indexes
+
+
+
+
+
+
